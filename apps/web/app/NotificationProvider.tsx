f399c7a9ae7e714bfee19ed3ba9cdd509685f2f1
millerm30 <millergm30@gmail.com>
2025-01-31 14:54:52 -0330

    remove credentials

--- apps/web/app/NotificationProvider.tsx
+++ apps/web/app/NotificationProvider.tsx
@@ -5,8 +5,8 @@ import { NotificationAPIProvider } from '@notificationapi/react';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NotificationAPIProvider
-      clientId='0dcxf5zrhfdrcbibobhn8y5um0'
-      userId='millerm30@icloud.com'
+      clientId='Add your client id here'
+      userId='Add your user id here'
       playSoundOnNewNotification={true}
       customServiceWorkerPath="service/notificationapi-service-worker.js"
     >
